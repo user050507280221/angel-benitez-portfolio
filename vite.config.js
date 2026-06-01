36314da16495b0090a7e190bf7bfb40634a6edde
user050507280221 <angelbenitezayado477@gmail.com>
2026-06-01 19:40:59 +0800

    Switch to cloud build pipeline compilation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Automatically switches paths based on whether GitHub Actions or Vercel is building the app
-  base: process.env.GITHUB_ACTIONS ? '/angel-benitez-portfolio/' : '/',
+  // This tells GitHub Pages to map URLs to your exact repository folder
+  base: '/angel-benitez-portfolio/',
 })
